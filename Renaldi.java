0ffa62e634dcdbf73a48c9005d01dac0c6875fc9
MUhammmadRenaldiHimawan <43022368+MUhammmadRenaldiHimawan@users.noreply.github.com>
2018-09-06 09:57:29 +0700

    Add files via upload

---
+++ Renaldi.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author asus
+ */
+public class Renaldi {
+    
+/**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int r=5;
+        for (int t=1;t<=5;t++){
+            for(int z=1;z<=t;z++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
+
